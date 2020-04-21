3bb745f377886f9870b97ca8722911ab9a9d535e
Pedro Bravo <pedro.bravoa@yahoo.com>
2020-04-21 14:56:30 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction("Pedro")
+
+introduction_with_language("Pedro", "Ruby")
+
